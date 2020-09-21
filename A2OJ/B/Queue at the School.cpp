c2c5636aa326e3717854ed6017bdcb4489156979
Dhruv Kothari <kotharidhruv25@gmail.com>
2020-09-21 07:24:45 +0530

    added B a2oj

--- A2OJ/B/Queue at the School.cpp
+++ A2OJ/B/Queue at the School.cpp
@@ -5,12 +5,18 @@ using namespace std;
 
 int main()
 {
-	int t;
-	cin>>t;
-	while(t--)
-	{
-		
+	int n , t;
+	cin>>n>>t;
+	string s;
+	cin>>s;
+	while(t--){
+		for(int i = 0; i<n-1; i++){
+			if(s[i] == 'B' && s[i+1] == 'G'){
+				swap(s[i], s[i+1]);
+				i++;
+			}
+		}
 	}
-
+	cout<<s;
 	return 0;
 }
