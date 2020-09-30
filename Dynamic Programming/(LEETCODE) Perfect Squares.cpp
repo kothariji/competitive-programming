4bdc7585927b6c579230f9fffce3e88c86bf499b
ashneg <500060167@stu.upes.ac.in>
2020-10-01 01:48:19 +0530

    Added (LEETCODE) Perfect Squares.cpp

---
+++ Dynamic Programming/(LEETCODE) Perfect Squares.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numSquares(int n) {
+    static vector<int> dp {0};
+    while (dp.size() <= n) {
+        int m = dp.size(), squares = INT_MAX;
+        for (int i=1; i*i<=m; ++i)
+            squares = min(squares, dp[m-i*i] + 1);
+        dp.push_back(squares);
+    }
+    return dp[n];
+}
+};
