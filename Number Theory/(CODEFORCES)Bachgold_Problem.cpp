8efc39d2eba76120f464345db811ca35e206eb2b
harish3600 <harishumasankar@students.iiit.ac.in>
2020-10-06 23:56:56 +0400

    Added solution to CODEFORCES: Bachgold_Problem

--- Number Theory/(CODEFORCES)Bachgold_Problem.cpp
+++ Number Theory/(CODEFORCES)Bachgold_Problem.cpp
@@ -5,25 +5,22 @@ using namespace std;
 #define fast            std::ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define pb              push_back
 #define vec             vector<ll>
-#define inf				1e9
 #define take_n_inputs   ll n; cin>>n; while(n--){ll ele; cin>>ele; v.pb(ele);}
-#define sort_asc    	sort(v.begin(),v.end());
-#define sort_desc 		sort(v.begin(),v.end(),greater<ll>());
-#define yes 			cout << "YES" << endl
-#define no 				cout << "NO" << endl
-#define len(c) 			c.size()
-#define all(c) 			c.begin(), c.end()
-#define rall(c) 		c.rbegin(), c.rend()
-#define trav(x,c) 		for(auto &x : c)
-#define rep(a,n) 		for(ll i = a ; i < n ; ++i)
-#define rrep(a,n) 		for(ll i = n-1 ; i >= a ; --i)
+#define inf             1e9
+#define sort_asc        sort(v.begin(),v.end());
+#define sort_desc       sort(v.begin(),v.end(),greater<ll>());
+#define yes             cout << "YES" << endl
+#define no              cout << "NO" << endl
+#define len(c)          c.size()
+#define all(c)          c.begin(), c.end()
+#define rall(c)         c.rbegin(), c.rend()
+#define trav(x,c)       for(auto &x : c)
+#define rep(a,n)        for(ll i = a ; i < n ; ++i)
+#define rrep(a,n)       for(ll i = n-1 ; i >= a ; --i)
 int main()
 {
   fast
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif  
+    
     ll n; cin>>n;
     if(n%2)
     {
