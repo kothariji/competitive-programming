7c16105b69e31cb2a059edde8930eaa0a53db218
Vivek-Hotti <vivekshotti@gmail.com>
2021-03-07 12:10:45 +0530

    Question Code LUCKFOUR 's solution added.

--- Codechef solutions/LUCKFOUR.cpp
+++ Codechef solutions/LUCKFOUR.cpp
@@ -1,18 +1,20 @@
-#include <iostream>
-using namespace std;
+// This problem can be found on this URL : https://www.codechef.com/problems/LUCKFOUR
+#include<stdio.h>
+#include<string.h>
 
-int main() {
-    int t, n, c;
-	cin >> t;
-	while (t--) {
-		cin >> n;
-		c = 0;
-		while (n) {
-			c = n%10==4 ? c+1 : c;
-			n /= 10;
-		}
-		
-		cout << c << endl;
-	}
-	return 0;
+int main()
+{
+    int t; char a[15];
+    scanf("%d",&t);
+    while(t--)
+    {
+        scanf("%s",&a);
+        int c=0,k;
+        for(k=0;k<strlen(a);k++)
+        {
+            if(a[k]=='4') c++;
+        }
+        printf("%d\n",c);
+    }
 }
+ 
