1c5c93f7d2bfce608a8b9a96cfe580c4e2b03405
kothariji <kotharidhruv25@gmail.com>
2021-04-07 22:26:32 +0530

    🌳(LEETCODE) Serialize and Deserialize

---
+++ Leetcode - Top Interview Questions/297. Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,42 @@
+class Codec
+{
+public:
+    // Encodes a tree to a single string.
+    string serialize(TreeNode *root)
+    {
+        if (root == nullptr)
+            return "#";
+        return to_string(root->val) + "," + serialize(root->left) + "," + serialize(root->right);
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode *deserialize(string data)
+    {
+        return mydeserialize(data);
+    }
+    TreeNode *mydeserialize(string &data)
+    {
+        if (data[0] == '#')
+        {
+            if (data.size() > 1)
+                data = data.substr(2);
+            return nullptr;
+        }
+        else
+        {
+            TreeNode *node = new TreeNode(helper(data));
+            node->left = mydeserialize(data);
+            node->right = mydeserialize(data);
+            return node;
+        }
+    }
+
+private:
+    int helper(string &data)
+    {
+        int pos = data.find(',');
+        int val = stoi(data.substr(0, pos));
+        data = data.substr(pos + 1);
+        return val;
+    }
+};
