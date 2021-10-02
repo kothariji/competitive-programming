3ccc655ec74e7ba0ce0c2ede0e012cbbebdecca7
amanprajapati25 <84510871+amanprajapati25@users.noreply.github.com>
2021-10-03 02:28:40 +0530

    Create reverse_a_doublylinkedlist.cpp
    reverse a doubly linked list using c++

---
+++ reverse_a_doublylinkedlist.cpp
@@ -0,0 +1,129 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+
+Node *newNode(int data)
+{
+    Node *temp=new Node(data);
+    
+    return temp;
+}
+
+
+
+
+void displayList(Node *head)
+{
+    //Head to Tail
+    while(head->next)
+    {
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    cout<<head->data;
+    
+    
+    
+}
+
+
+int getLength(Node * head)
+{
+    Node *temp=head;
+    
+    int count=0;
+    while(temp->next!=head)
+    {
+        count++;
+        temp=temp->next;
+    }
+    return count+1;
+}
+
+
+
+
+bool verify(Node* head)
+{
+    int fl=0;
+    int bl=0;
+    
+    Node *temp=head;
+    
+    while(temp->next)
+    {
+        temp=temp->next;
+        fl++;
+    }
+    
+    while(temp->prev)
+    {
+        temp=temp->prev;
+        bl++;
+    }
+    
+    return fl==bl;
+}
+
+
+ // } Driver Code Ends
+/*
+struct Node
+{
+    int data;
+    Node * next;
+    Node * prev;
+    Node (int x)
+    {
+        data=x;
+        next=NULL;
+        prev=NULL;
+    }
+        
+};
+*/
+Node* reverseDLL(Node * head)
+{
+    //Your code here
+    Node* curr=head;
+    
+    if (head==NULL || head->next==NULL)
+        return head;
+        
+    while(curr->next != NULL){
+         curr=curr->next;
+    }
+    
+   head=curr;
+    curr->next=curr->prev;
+    curr->prev=NULL;
+    
+    curr=curr->next;
+    while(curr->prev!=NULL){
+        Node* temp=curr->next;
+        curr->next=curr->prev;
+        curr->prev=temp;
+        
+        curr=curr->next;
+    }
+    curr->prev=curr->next;
+    curr->next=NULL;
+    
+    return head;
+}
+
