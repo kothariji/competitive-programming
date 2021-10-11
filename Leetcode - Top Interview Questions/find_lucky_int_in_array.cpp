684c2358a60da9c713fed9fdb18358cd2374aebe
Anweasha Saha <anweashasaha2000@gmail.com>
2021-10-11 12:54:58 +0530

    added leetcode_1394 find lucky integer

---
+++ Leetcode - Top Interview Questions/find_lucky_int_in_array.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        int len = arr.size();
+        int frequency;
+        int current_luky = INT_MIN;
+        for(int i =0; i < len; i++)
+        {
+            frequency =0;
+            for(int j = 0; j < len; j++)
+            {
+                if(arr[i] == arr[j])
+                    frequency++;
+            }
+            if(frequency == arr[i])
+            {
+                if(arr[i]> current_luky)
+                    current_luky = arr[i];
+            }
+        }
+        
+        if(current_luky == INT_MIN)
+            return -1;
+        return current_luky;  
+    }
+};
