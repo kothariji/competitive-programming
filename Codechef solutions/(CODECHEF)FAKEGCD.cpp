0aa4a807821b777ea890284b2bb9939aab5c3b13
Siddhant <siddhantmanze@gmail.com>
2021-10-29 22:24:31 +0530

    Create (CODECHEF)FAKEGCD.cpp

---
+++ Codechef solutions/(CODECHEF)FAKEGCD.cpp
@@ -0,0 +1,20 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        for(int i=n; i>=1; i--)
+        {
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
