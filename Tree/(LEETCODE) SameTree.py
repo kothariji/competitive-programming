9d14f8952660b9c6c1de639cbbdbe2fc90f608d7
Saiyam Jain <59705243+CyberSaiyam@users.noreply.github.com>
2022-10-02 00:10:24 +0530

    Added SameTree Python Solution

---
+++ Tree/(LEETCODE) SameTree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+
+        if not p or not q:
+            return False
+
+        if p.val != q.val:
+            return False
+
+        return (self.isSameTree(p.left, q.left)) and (self.isSameTree(p.right, q.right))
