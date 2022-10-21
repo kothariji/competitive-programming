a15dc04f387bfb1604a7f539fc05b491bcd07dd3
K Shashankh Vasudev <63902267+KanniShashankh@users.noreply.github.com>
2022-10-21 08:30:27 +0530

    Update (CODEFORCES)cAPS_lOCK.cpp
    Faster time complexity as the loop ends as soon as a lowercase letter is detechted.

--- String/(CODEFORCES)cAPS_lOCK.cpp
+++ String/(CODEFORCES)cAPS_lOCK.cpp
@@ -1,60 +1,72 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
+
 #define pb push_back
-#define loop(a, b, c) for (int i = a; i < b; i += c)
-#define tc       \
-    long long t; \
-    cin >> t;    \
-    while (t--)
-#define fast std::ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
-#define print(x) cout << x << nline
-const int N = 1e6 + 1;
-#define mod 1000000007
+#define sz(x) (int)(x).size()
+#define forn(i,n) for(int i=0;i<(n);++i)
+
+//#define ull unsigned long long int
+#define int long long int
+#define all(x)             (x).begin(),(x).end()
+#define MOD 1000000007
+#define mac 998244353
+#define mp make_pair
+#define loop(i,a,b) for (int i=a;i<=b;i++)
+#define ss second
+#define ff first
+
+const char nl = '\n';
+const char sp = ' ';
+const int mod = 1000000007;
+//const int lol = 10007;
+
+ 
  
 void solve()
 {
-    string s;
-    cin >> s;
-    string t = s;
-    int a = 0;
- 
-    for (int i = 0; i < s.length(); i++)
+    string s; cin>>s;
+    int n = s.size();
+    bool k = true;
+
+    for(int i=1;i<n && k;i++)
     {
-        if (s[i] >= 'A' and s[i] <= 'Z')
+        if(islower(s[i]))
         {
-            a++;
-        }
+            k = false;  
+        } 
     }
-    if (a == s.length())
+    bool fail = false;
+    if(s[0]==toupper(s[0])) 
     {
-        for (int i = 0; i < s.length(); i++)
-        {
-            s[i] += 32;
-        }
+        fail=true;
     }
-    else if (a == s.length() - 1)
+    if(k)
     {
-        if (s[0] >= 'a' and s[0] <= 'z')
-        {
-            s[0] -= 32;
-            for (int i = 1; i < s.length(); i++)
-            {
-                s[i] += 32;
-            }
-        }
+        for(auto &i : s) i = tolower(i);
+        if(!fail) s[0] = toupper(s[0]);
     }
- 
-    cout<<s<<"\n";
+    cout<<s<<nl;
 }
  
-signed main()
+ 
+ 
+
+
+signed main() 
 {
-    fast;
-    // tc
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int T=1; 
+    //cin >> T;
+
+    
+    for(int tc = 1; tc <= T; tc++)
     {
-        solve();
+        //cout << "Case #" << tc << ": ";
+        solve(); 
     }
+   
+    return 0;  
 }
- 
-// cAPSlOCK --> no change
+
